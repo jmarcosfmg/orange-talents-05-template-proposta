2ee6ff6dfdfca29c63f6cbdf157a8fbfe85b529f
jmarcosfmg <jmarcosfmg@gmail.com>
2021-06-11 13:22:30 -0300

    adição de segurança com keycloak

---
+++ src/main/java/br/com/zup/oragetalents/proposta/misc/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package br.com.zup.oragetalents.proposta.misc.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(web);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().authorizeRequests().antMatchers("/proposta**").hasAuthority("USER")
+				.antMatchers("/biometria**").hasAuthority("USER").anyRequest().authenticated().and()
+				.oauth2ResourceServer().jwt().jwtAuthenticationConverter(getJwtAuthenticationConverter());
+
+		// super.configure(http);
+	}
+
+	private JwtAuthenticationConverter getJwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+		converter.setAuthoritiesClaimName("authorities");
+		converter.setAuthorityPrefix("");
+		JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+		authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+		return authenticationConverter;
+	}
+}
